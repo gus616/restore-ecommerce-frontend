82600498922721cdd827d7eaf4507e54d7303008
Gus Vc <guzz.vc@gmail.com>
2025-07-08 13:31:54 -0600

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline text-purple-500">
+      ReStore
+    </h1>
+    </>
+  )
+}
+
+export default App
