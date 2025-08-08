6b4bfec60667c3e2882e5cc706c5231467df01fe
Gus Vc <guzz.vc@gmail.com>
2025-08-08 00:47:05 -0600

    chore: fix jsx errors

--- src/features/catalog/components/__tests__/ProductList.test.tsx
+++ src/features/catalog/components/__tests__/ProductList.test.tsx
@@ -4,28 +4,31 @@ import { MemoryRouter } from "react-router-dom";
 
 describe('ProductList', () => {
 
-    
+
     it('render the list of products', () => {
         const products = [
             {
                 id: 1,
-                name: 'Death Stranding 2',
-                description: 'An open-world action game',
-                brand: 'Kojima Productions',
-                price: 59.99,
-                pictureUrl: 'https://example.com/death-stranding-2.jpg',
-                quantityInStock: 100,
-                type: 'Video Game'
+                name: "Sample",
+                description: "desc",
+                brand: "brand",
+                price: 100,
+                pictureUrl: "url",
+                quantityInStock: 5,
+                type: "type",
+                images: ["image1.jpg", "image2.jpg"], // <-- add this property!
+
             },
             {
                 id: 2,
-                name: 'The Last of Us Part II',
-                description: 'A narrative-driven action-adventure game',
-                brand: 'Naughty Dog',
-                price: 49.99,
-                pictureUrl: 'https://example.com/last-of-us-part-ii.jpg',
-                quantityInStock: 50,
-                type: 'Video Game'
+                name: "Sample",
+                description: "desc",
+                brand: "brand",
+                price: 100,
+                pictureUrl: "url",
+                quantityInStock: 5,
+                type: "type",
+                images: ["image1.jpg", "image2.jpg"], // <-- add this property!
             }
         ];
 
